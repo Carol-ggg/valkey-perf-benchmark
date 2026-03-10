d0a975453856b684d770c2489a788d145c391903
Ram Prasad Voleti <ramvolet@amazon.com>
2026-03-10 21:41:41 +0000

    Add description to metrics
    Add test descriptions to metrics
    
    Signed-off-by: Ram Prasad Voleti <ramvolet@amazon.com>

--- valkey_benchmark.py
+++ valkey_benchmark.py
@@ -974,6 +974,10 @@ class ClientRunner:
                     metrics["config_set"] = config_set
                     if scenario.get("dataset"):
                         metrics["dataset"] = scenario["dataset"]
+                    if scenario.get("description"):
+                        metrics["description"] = scenario["description"]
+                    if scenario.get("config_description"):
+                        metrics["config_description"] = scenario["config_description"]
                     return metrics
 
         except Exception as e:
